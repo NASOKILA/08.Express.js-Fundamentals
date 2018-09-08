84b5c7501ecfa9def309a3c2296eb2bbb94a42b4
NASOKILA <atanasskambitovv@gmail.com>
2018-09-08 08:37:31 +0300

    View Engines Exercises

--- 13.View Engines Exercises/schemas/bookSchema.js
+++ 13.View Engines Exercises/schemas/bookSchema.js
@@ -1,4 +1,3 @@
-
 let mongoose = require('mongoose');
 
 module.exports = 
@@ -8,20 +7,3 @@ module.exports =
     poster: { type: String, requred: true },
     author: { type: String }
 });
-
-/*
-let mongoose = require('mongoose');
-
-//pravim si modela
-let bookModel = 
-    mongoose.model('Book', {
-    title: { type: String, requred: true },
-    author: { type: String},
-    year: { type: Date},
-    poster: { type: String, requred: true },
-});
-
-//registrirame go v mongoose i go exportvame
-module.exports = mongoose.model('Book', bookModel);
-
- */
