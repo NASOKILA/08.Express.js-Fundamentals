2a2c1a144e5ac37928e865b22f61a02f5319722d
NASOKILA <atanasskambitovv@gmail.com>
2018-09-08 08:13:00 +0300

    Node.js Common Models

--- 06.Node.js Common Models/pipeServer.js
+++ 06.Node.js Common Models/pipeServer.js
@@ -11,12 +11,10 @@ server.on('request', (req, res) => {
     src.on('data', data => res.write(data));
     src.on('end', () => res.end());
     
-    //TOZI NACHIN E PO LESEN I PO BURZ DORI I OT TOZI VUV server.js FAILA.
     server.on('request', (req, res) => {
         const src = fs.createReadStream('./testDir/bigFile.txt');
         src.pipe(res);
     });
-
 });
 
 server.listen(port);
--- 06.Node.js Common Models/zipFile.js
+++ 06.Node.js Common Models/zipFile.js
@@ -9,9 +9,7 @@ let writeStream = fs.createWriteStream('./testDir/bigFile.txt.gz');
 
 let gzip = zlib.createGzip();
 
-//TOVA E ZA UNZIPVANE
 let unzip = zlib.createGunzip();
 
 readStream.pipe(gzip).pipe(writeStream);
 
-//SLED KATO GO KOMPRESIRAME FAILUT STAVA MNOGO PO MALUK VIJ V SAMATA PAPKA KOLKO E RAZLIKATA V GOLEMINATA.
