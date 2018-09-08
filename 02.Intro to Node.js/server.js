7f853426fff7fb40939dbf181cdbec90d7b99acf
NASOKILA <atanasskambitovv@gmail.com>
2018-09-08 07:53:56 +0300

    Intro to Node.js

--- 02.Intro to Node.js/server.js
+++ 02.Intro to Node.js/server.js
@@ -1,23 +1,12 @@
 
 
-//purvo trqbva da si vkluchim http modula koito e vgraden v node.js
 const http = require('http'); 
-
-//s http modula mojem da otgovarqme na vsqkukvi zaqvki.
-//http modula ima funkciq .createServer() s koqto suzdavame web server 
-//.createServer() funkciqta e callBack funkciq koqto shte se izvika kogato poluchim zaqvka
-//priema request - sudurja headeri, metadanni i dr, i response - koito e edin obekt ot 'http' modula 
-    //s koito nie pishem otgovorite
-//v req imame vsichko za pusnatata zaqvka kum nashiq server.
-
-//sus res.write('...'); pishem otgovor vurhu responsa
-//res.end() izprasta responsa na klienta    
     
 let app = http.createServer((req, res) => {
     
     res.write('Hi, I am a new server!' + '\n');
-    res.write('Method: ' + req.method + '\n'); //metodut
-    res.write('Url: ' + req.url + '\n'); // linkut
+    res.write('Method: ' + req.method + '\n'); 
+    res.write('Url: ' + req.url + '\n'); 
     
     if(req.method === 'GET')
     {
@@ -46,10 +35,6 @@ let app = http.createServer((req, res) => {
 });
 
 let port = '5000';
-app.listen(port); //sus .listen(portNumber) GO PUSKAME kato mu kazvame na koi port da slusha.
-
+app.listen(port); 
 
 console.log(`Node.js server running on port ${port}`);
-
-//za dad refreshnem servera trqbva da go restartirame OBACHE 
-//Ima neshta koito ni refreshvat servera avtomatichno sled vsqko seivane
